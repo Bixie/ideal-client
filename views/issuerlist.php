588985a081759e86f08c2545ff706433a6f5ff21
malles <matthijs@bixie.nl>
2016-03-24 21:54:52 +0100

    fix action

--- views/issuerlist.php
+++ views/issuerlist.php
@@ -6,15 +6,15 @@
 
 ?>
 
-<form class="uk-form">
+<form class="uk-form" action="<?=$action?>" method="post">
 
-	<label for="issuer_id">Please select your bank</label>
+	<label for="issuer_id">Please select your bank</label><br>
 
 	<select name="issuer_id" id="issuer_id">
 		<?php foreach ($issuers as $value => $label) : ?>
 			<option value="<?= $value ?>"><?= $label ?></option>
 		<?php endforeach; ?>
-	</select>
+	</select><br>
 
 	<button class="uk-button uk-margin">Continue<i class="uk-icon-angle-double-right uk-margin-small-left"></i></button>
 </form>
